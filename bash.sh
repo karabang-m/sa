696edd9fa176b37ea0a8f70703aacae41a3e355a
karabang-m <117525829+karabang-m@users.noreply.github.com>
2022-11-05 21:14:41 +0545

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -4,6 +4,7 @@ apt install curl -y
 apt-get install software-properties-common -y
 apt upgrade -y
 apt update -y 
+apt install git -y
 apt install  python3 pip -y 
 apt update -y 
 apt-get install gnupg2 gnupg gnupg1   -y
